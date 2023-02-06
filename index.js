2474c472f33b24da8bf1ef0dd82112512079afed
pozafly <pozafly@gmail.com>
2023-02-06 15:58:57 +0900

    start source

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * "Hello {이름}"을 반환하는 메서드
+ *
+ * @param {string} name 사용자가 반환하길 원하는 이름을 출력한다.
+ * @returns {string} Hello + name
+ */
+function hello(name) {
+  return 'Hello' + name + '!';
+}
